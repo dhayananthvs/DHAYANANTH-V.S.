1d073f543b788b148aae03468722fa290ef8f104
dhayananthvs <35652205+dhayananthvs@users.noreply.github.com>
2018-01-26 10:52:09 +0000

    Create even between intervals.c

---
+++ even between intervals.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main() 
+{
+int i,a,b,c;
+printf("enter the limits");
+scanf("%d %d",&a,&b);
+printf("the values are");
+for(i=a;i<b;i++)
+if(i%2==0)
+printf(" \t %d",i);
+}
