1ee6aa20f352c21689fdefc06a173d5beba5f87a
dhayananthvs <35652205+dhayananthvs@users.noreply.github.com>
2018-03-02 15:12:58 +0530

    Create power of 2 correct.c

---
+++ power of 2 correct.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+int a,b,c;
+scanf("%d",&a);
+b=sqrt(a);
+c=pow(b,2);
+if(c==a)
+{
+printf("\n %d is the power of 2",a);
+}
+else
+{
+    printf("\n %d is not the power of 2",a);
+}}
