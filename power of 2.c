afd59beb0699c5ee6d19f088adc2ac2052f1336b
dhayananthvs <35652205+dhayananthvs@users.noreply.github.com>
2018-03-02 14:49:32 +0530

    Create power of 2.c

---
+++ power of 2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+float a,b,c;
+scanf("%f",&a);
+b=sqrt(a);
+c=pow(b,2);
+if(c==a)
+{
+printf("\n %f is the power of 2",a);
+}
+else
+{
+    printf("\n %f is not the power of 2",a);
+}}
