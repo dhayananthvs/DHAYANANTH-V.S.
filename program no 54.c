54f2d16d3a9f52db8a6b5d249b71991ba7110f9b
dhayananthvs <35652205+dhayananthvs@users.noreply.github.com>
2018-03-15 16:42:03 +0530

    Create program no 54.c

---
+++ program no 54.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("\n Enter the number\t");
+scanf("%d",&n);
+if(n%2==0)
+{
+    printf("\n %d",n);
+}
+else
+{
+    n-=1;
+    printf("\n %d",n);
+}}
